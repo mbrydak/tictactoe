ebdf67c8d5c8cc44945e4fee6c2e7b798246955d
wbrydak <brydakwojciech@gmail.com>
2020-03-07 14:21:51 +0100

    Added checkWin

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -1,7 +1,19 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+
+const WIN_COMBO = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('game-board');
+const gameBoard = document.getElementById('game-board');
 let circleTurn;
 
 function changeTurn() {
@@ -11,18 +23,28 @@ function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
 }
 function setBoardHover() {
-  board.classList.remove(X_CLASS);
-  board.classList.remove(CIRCLE_CLASS);
+  gameBoard.classList.remove(X_CLASS);
+  gameBoard.classList.remove(CIRCLE_CLASS);
   if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
+    gameBoard.classList.add(CIRCLE_CLASS);
   } else {
-    board.classList.add(X_CLASS);
+    gameBoard.classList.add(X_CLASS);
   }
+}
+function checkWin(currentClass) {
+  return WIN_COMBO.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList(currentClass);
+    });
+  });
 }
 function hadnleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
+  if (checkWin()) {
+    console.log('winner');
+  }
   changeTurn();
   setBoardHover();
 }
